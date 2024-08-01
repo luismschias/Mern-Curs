89523b205c5edd2d145b4f6ec74cdf6ddf38acf2
luis miguel schias <luismschias@hotmail.com>
2024-08-01 17:45:01 -0300

    Mern Project
    Meu projeto mern

---
+++ util/location.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const HttpError = require('../models/http-error');
+
+const API_KEY = 'AIzaSyB1K4IZuwST7cOKXGwiy0M2JFqROccReY0';
+
+async function getCoordsForAddress (address) {
+    // return {
+    //     lat: 40.7484474,
+    //     lng: -73.9871516
+    // };
+
+   const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
+
+   const data = response.data
+
+   if(!data|| data.status === 'ZERO_RESULTS'){
+    const error = new HttpError('Could not find location for the specified address.', 422)
+    
+    throw error;
+  }
+  
+  const coordinates = data.results[0].geometry.location;
+
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
